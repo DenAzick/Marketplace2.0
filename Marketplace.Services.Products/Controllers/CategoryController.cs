39b39b042248ea40d935aa03bb3bf15baba6a72f
Kasimbekov Azizbek <azizbekkasimbekov@gmail.com>
2023-06-13 15:51:13 +0500

    544

---
+++ Marketplace.Services.Products/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Marketplace.Services.Products.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Services.Products.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory(CreateProductModel model)
+    {
+
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateCategory(CreateProductModel model)
+    {
+
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCategories(CreateProductModel model)
+    {
+
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCategoriesById(CreateProductModel model)
+    {
+
+    }
+
+    [HttpDelete]
+    public async Task DeleteCategory()
+    {
+
+    }
+}
---
+++ Marketplace.Services.Products/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Marketplace.Services.Products.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Services.Products.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(CreateProductModel model)
+    {
+
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateProduct(CreateProductModel model)
+    {
+
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProducts(CreateProductModel model)
+    {
+
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProductsById(CreateProductModel model)
+    {
+
+    }
+
+    [HttpDelete]
+    public async Task DeleteProduct()
+    {
+
+    }
+
+}
