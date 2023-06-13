d8b788a260f248e699b497bd6545c083701a0a9c
Kasimbekov Azizbek <azizbekkasimbekov@gmail.com>
2023-06-13 15:43:31 +0500

    erqawer

---
+++ IdentityMudBlazor/Models/LoginUserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityMudBlazor.Models;
+
+public class LoginUserModel
+{
+    public  string Password { get; set; }
+    public  string UserName { get; set; }
+}
--- IdentityMudBlazor/Program.cs
+++ IdentityMudBlazor/Program.cs
@@ -2,12 +2,15 @@ using IdentityMudBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7032") });
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
